d852c64458abd9019234d6a7b65dcbdb9faf90f1
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-08-19 17:55:01 -0700

    added homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class HomePage extends Component {
     state = {
         todos: [],
     }
 
-    componentDidMount = async () => {
-        if (!this.props.token) {
-          this.props.history.push('/login');
-        } 
-    }
+    // componentDidMount = async () => {
+    //     if (!this.props.token) {
+    //       this.props.history.push('/login');
+    //     } 
+    // }
     render() {
         return (
             <div>
-                Hey its time to pretend you're not procrastinating! 
+                Hey its time to pretend you're not procrastinating! Sign in or sign up to get started.
+                <Link to='/login'>Log in or Sign up</Link>
             </div>
         )
     }
